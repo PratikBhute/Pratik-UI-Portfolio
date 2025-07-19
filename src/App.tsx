7a0bc9819476cae92871f93e31962279a44819ff
Pratik Bhute <pratik.bhute@ascendion.com>
2025-07-19 22:05:47 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       {/* <AppProvider> */}
         <BrowserRouter>
           <Routes>
-            <Route path="" element={<Index />} />
+            <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
